20b7fc658abb712973d99d1e659c53720606f86c
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-07-10 19:37:54 -0500

    primer componente

--- App.js
+++ App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const Texto = ({ texto }) => {
-  return <Text>{texto}</Text>;
-};
+// class Texto extends Component {
+//   state = {
+//     texto: "Hola Mundo",
+//     presionado: false,
+//     color: styles.text2
+//   };
+//   handlePress = () => {
+//     const { presionado } = this.state;
+//     if (!presionado) {
+//       this.setState({
+//         texto: "Adios Mundo Cruel",
+//         presionado: true,
+//         color: styles.text1
+//       });
+//     } else {
+//       this.setState({
+//         texto: "Hola Mundo",
+//         presionado: false,
+//         color: styles.text2
+//       });
+//     }
+//   };
+//  render() {
+//     const { texto, color } = this.state;
+
+//     return (
+//       <Text onPress={this.handlePress} style={color}>
+//         {texto}
+//       </Text>
+//     );
+//   }
+// }
 
 export default function App() {
-  console.log("Hola Mundo....");
   return (
     <View style={styles.container}>
-      <Texto texto="Hola mundo" />
-      <Texto texto="Adios Mundo" />
-      <Texto texto="Chanchito" />
+      <Texto />
+      <Texto />
+      <Texto />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text1: {
+    color: "red"
+  },
+  text2: {
+    color: "black"
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "left",
     justifyContent: "center"
   }
 });
